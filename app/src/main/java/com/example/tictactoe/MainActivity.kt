0460fcbf1098397b2a7d41e0cf768b3da04e8b96
mdrt1395 <marcodavidrt95@gmail.com>
2023-07-16 15:50:31 -0600

    Started from scratch, so I guess this is v2.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -6,24 +6,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var x1y1: Button
-    private lateinit var x1y2: Button
-    private lateinit var x1y3: Button
-    private lateinit var x2y1: Button
-    private lateinit var x2y2: Button
-    private lateinit var x2y3: Button
-    private lateinit var x3y1: Button
-    private lateinit var x3y2: Button
-    private lateinit var x3y3: Button
-    private var m1: Int = 0
-    private var m2: Int = 0
-    private var m3: Int = 0
-    private var m4: Int = 0
-    private var m5: Int = 0
-    private var m6: Int = 0
-    private var m7: Int = 0
-    private var m8: Int = 0
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var a0: Button
+    private lateinit var a1: Button
+    private lateinit var a2: Button
+    private lateinit var a3: Button
+    private lateinit var a4: Button
+    private lateinit var a5: Button
+    private lateinit var a6: Button
+    private lateinit var a7: Button
+    private lateinit var a8: Button
 
 
 
@@ -31,32 +23,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        x1y1 = findViewById(R.id.x1y1)
-        x1y2 = findViewById(R.id.x1y2)
-        x1y3 = findViewById(R.id.x1y3)
-        x2y1 = findViewById(R.id.x2y1)
-        x2y2 = findViewById(R.id.x2y2)
-        x2y3 = findViewById(R.id.x2y3)
-        x3y1 = findViewById(R.id.x1y1)
-        x3y2 = findViewById(R.id.x1y2)
-        x3y3 = findViewById(R.id.x1y3)
 
-        m1 = 0
-        m2 = 0
-        m3 = 0
-        m4 = 0
-        m5 = 0
-        m6 = 0
-        m7 = 0
-        m8 = 0
+        a0 = findViewById(R.id.a0)
+        a1 = findViewById(R.id.a1)
+        a2 = findViewById(R.id.a2)
+        a3 = findViewById(R.id.a3)
+        a4 = findViewById(R.id.a4)
+        a5 = findViewById(R.id.a5)
+        a6 = findViewById(R.id.a6)
+        a7 = findViewById(R.id.a7)
+        a8 = findViewById(R.id.a8)
 
-
+        a0.setOnClickListener(this)
+        a1.setOnClickListener(this)
+        a2.setOnClickListener(this)
+        a3.setOnClickListener(this)
+        a4.setOnClickListener(this)
+        a5.setOnClickListener(this)
+        a6.setOnClickListener(this)
+        a7.setOnClickListener(this)
+        a8.setOnClickListener(this)
 
         }
 
-    private fun onButtonClick(view: View) {
-        val p1Move = 1
-        m1.plus(p1Move)
+    override fun onClick(p0: View?) {
+        var btnClicked = findViewById<Button>(p0!!.id)
+        btnClicked.text = "0"
+
     }
 }
 
